5b42d476027c7d636893416c85c308552b3284e0
George Gray <george.gray@generalassemb.ly>
2021-02-22 08:14:31 +0000

    yargs

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,39 @@
 #! /usr/bin/env node
 
-import { Command } from "commander";
+import * as yargs from "yargs";
 
 import main from "./main";
-import version from "./version";
 
-const cli = new Command();
-
-interface InterfaceCLI {
-  debug?: boolean;
-  date?: string;
-}
-
-cli
-  .option("-v, --version", "show version", version, "")
-  .option("-D, --debug", "output extra debugging")
-  .option("-d, --date <type>", "search by date (defaults to today)");
-
-cli.parse(process.argv);
-const options: InterfaceCLI = cli.opts();
+const args = yargs
+  .usage("Usage: dtd <command> [options]")
+  .command("dtd", "Display sunrise & sunset times")
+  .example(
+    "dtd -d '2021-08-12'",
+    "display sunrise & sunset times for a given day"
+  )
+  .alias("version", "v")
+  .option("help", {
+    alias: "h",
+    type: "boolean",
+    description: "Show help",
+    default: false,
+  })
+  .option("date", {
+    alias: "d",
+    type: "string",
+    description: "Search by date",
+    default: "today",
+  })
+  .option("logging", {
+    alias: "l",
+    type: "boolean",
+    description: "Output extra logging info",
+    default: false,
+  }).argv;
 
 void (async () => {
   await main({
-    debug: !!options.debug,
-    date: options.date || "today",
+    debug: args.logging,
+    date: args.date,
   });
 })();
